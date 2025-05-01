760ab5a990f1bff2c36a7bdf89f80eb87dc69e1b
Gabriel Vitoria <gabriel.vitoria.ti@gmail.com>
2025-05-01 18:42:43 -0300

    #000 - Serviço para envio de mensagem para o rabbitmq

--- Financial.Service/NotificationEventService.cs
+++ Financial.Service/NotificationEventService.cs
@@ -1,7 +1,10 @@
 ﻿using Financial.Domain.Dtos;
 using Financial.Service.Interfaces;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Financial.Service
 {
@@ -10,19 +13,40 @@ namespace Financial.Service
 
         public async Task SendAsync(FinanciallaunchEvent financiallaunchEvent)
         {
+            try
+            {
 
-            var factory = new ConnectionFactory { HostName = "localhost" };
-            using var connection = await factory.CreateConnectionAsync();
-            using var channel = await connection.CreateChannelAsync();
+                var factory = new ConnectionFactory
+                {
+                    HostName = "localhost", // Alteração aqui
+                    Port = 5672,          // Alteração aqui
+                    UserName = "financial",
+                    Password = "financial"
+                };
+
+                using var connection = await factory.CreateConnectionAsync();
+                using var channel = await connection.CreateChannelAsync();
 
 
-            await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive: false, autoDelete: false,arguments: null);
+                await channel.QueueDeclareAsync(queue: "Financiallaunch", durable: false, exclusive: false, autoDelete: false, arguments: null);
 
-            const string message = "Hello World!";
-            var body = Encoding.UTF8.GetBytes(message);
+                var jsonMessage = JsonSerializer.Serialize(financiallaunchEvent);
 
-            await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "hello", body: body);
-            Console.WriteLine($" [x] Sent {message}");
+                var body = Encoding.UTF8.GetBytes(jsonMessage);
+
+                await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "Financiallaunch", body: body);
+
+            }
+            catch (BrokerUnreachableException ex)
+            {
+                Console.WriteLine($"Error connecting to RabbitMQ: {ex.Message}");
+                // Lógica para lidar com a falha de conexão (tentar novamente, logar o erro, etc.)
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An unexpected error occurred: {ex.Message}");
+                // Lógica para lidar com outros tipos de erros
+            }
 
         }
     }
