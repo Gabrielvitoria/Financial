ed5014b43467e796488ec796c0f6966b23c88f95
Gabriel Vitoria <gabriel.vitoria.ti@gmail.com>
2025-05-03 19:50:16 -0300

    #000 Add ignore

--- Financial.Report/Financial.Service/Interfaces/IFinanciallaunchService.cs
+++ Financial.Report/Financial.Service/Interfaces/IFinanciallaunchService.cs
@@ -1,7 +1,10 @@
-﻿namespace Financial.Service.Interfaces
+﻿using Financial.Common;
+
+namespace Financial.Service.Interfaces
 {
     public interface IFinanciallaunchService
     {
-
+        Task ProcessesFinancialLauchAsync(FinanciallaunchEvent financiallaunchEvent);
+        Task<string> GetSaldoDiarioAsync();
     }
 }
